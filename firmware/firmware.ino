b5f77ce78fa7e0bdc89da5523676509d8bcfaaf5
Andreas Wälchli <andreas.waelchli@me.com>
2025-02-17 18:45:16 +0100

    update firmware
    page commands were still missing in the committed version...

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -85,6 +85,10 @@ void processBuffer() {
     processReadCommand();
   } else if (buffer[0] == 'w') {
     processWriteCommand();
+  } else if (buffer[0] == 'p') {
+    processPageReadCommand();
+  } else if (buffer[0] == 'x') {
+    processPageWriteCommand();
   } else {
     digitalWrite(ERROR_LED, true);
     Serial.print("-SYNTAX ERROR: INVALID COMMAND: ");
@@ -100,6 +104,7 @@ void processSync() {
     Serial.print(buffer[i]);
   }
   Serial.println();
+  digitalWrite(ERROR_LED, false);
 }
 
 void processReadCommand() {
@@ -130,6 +135,42 @@ void processReadCommand() {
 
   // address OK, execute read
   read(adr);
+}
+
+void processPageReadCommand() {
+  if (buffer_length != 5) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: READ COMMAND REQUIRES EXACTLY 4 ADDRESS CHARS");
+    return;
+  }
+
+  // extract address
+  int adrHigh = hexToByte(buffer + 1);
+  int adrLow = hexToByte(buffer + 3);
+
+  if (adrHigh == -1 || adrLow == -1) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: ADDRESS REQUIRES 4 HEX DIGITS");
+    return;
+  }
+
+  // assemble address
+  int adr = ((adrHigh << 8) & 0xff00) | (adrLow & 0x00ff);
+
+  if (adr < 0 || adr >= 0x2000) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: ADDRESS MUST BE IN RANGE 0000..1FFF");
+    return;
+  }
+
+  if ((adr & 0x003f) > 0) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: ADDRESS MUST BE A PAGE START");
+    return;
+  }
+
+  // address OK, execute read
+  pageRead(adr);
 }
 
 void processWriteCommand() {
@@ -173,6 +214,55 @@ void processWriteCommand() {
   }
 
   write(adr, data & 0x00ff);
+}
+
+void processPageWriteCommand() {
+  if (buffer_length != 134) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: WRITE COMMAND REQUIRES EXACTLY 4 ADDRESS CHARS, 1 SEPARATOR AND 128 DATA CHARS");
+    return;
+  }
+
+  // extract address
+  int adrHigh = hexToByte(buffer + 1);
+  int adrLow = hexToByte(buffer + 3);
+
+  if (adrHigh == -1 || adrLow == -1) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: ADDRESS REQUIRES 4 HEX DIGITS");
+    return;
+  }
+
+  // assemble address
+  int adr = ((adrHigh << 8) & 0xff00) | (adrLow & 0x00ff);
+
+  if (adr < 0 || adr >= 0x2000) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: ADDRESS MUST BE IN RANGE 0000..1FFF");
+    return;
+  }
+
+  if ((adr & 0x003f) > 0) {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: ADDRESS MUST BE A PAGE START");
+    return;
+  }
+
+  // address OK
+  if (buffer[5] != ':') {
+    digitalWrite(ERROR_LED, true);
+    Serial.println("-SYNTAX ERROR: WRITE COMMAND NEEDS SEPARATOR AS SIXTH CHAR");
+    return;
+  }
+
+  // build data buffer
+  byte data[128];
+  for(int i = 0; i < 64; i++) {
+    data[i] = hexToByte(buffer + 6 + (2 * i));
+  }
+
+  pageWrite(adr, data);
+
 }
 
 // -------------------------------------------------------
@@ -223,7 +313,32 @@ void read(int address) {
   chipEnable(false);
 
   Serial.write("+");
+  if (readData < 16) {
+    Serial.print('0');
+  }
   Serial.println(readData, HEX);
+  digitalWrite(ERROR_LED, false);
+}
+
+void pageRead(int address) {
+  Serial.write("+");
+
+  chipEnable(true);
+  digitalWrite(OUTPUT_ENABLE_PIN, false);
+  for (int i = 0; i < 64; i++) {
+    selectAddress(address + i);
+    delay(1);
+    byte readData = DATA_BUS_I;
+    
+    if (readData < 16) {
+      Serial.print('0');
+    }
+    Serial.print(readData, HEX);
+  }
+  digitalWrite(OUTPUT_ENABLE_PIN, true);
+  chipEnable(false);
+
+  Serial.println();
   digitalWrite(ERROR_LED, false);
 }
 
@@ -263,12 +378,54 @@ void write(int address, byte data) {
   }
 }
 
+void pageWrite(int address, byte* data) {
+  chipEnable(true);
+  digitalWrite(WRITE_LED, true);
+
+  DATA_BUS_D = 0xff;
+  for (int i = 0; i < 64; i++) {
+    selectAddress(address + i);
+    DATA_BUS_O = *(data + i);
+    digitalWrite(WRITE_ENABLE_PIN, false);
+    digitalWrite(WRITE_ENABLE_PIN, true);
+  }
+  delay(10);
+
+  DATA_BUS_O = 0x00;
+  
+  digitalWrite(WRITE_LED, false);
+  DATA_BUS_D = 0x00;
+
+  // verify data
+  digitalWrite(OUTPUT_ENABLE_PIN, false);
+
+  for (int i = 0; i < 64; i++) {
+    selectAddress(address + i);
+    delay(1);
+    byte readData = DATA_BUS_I;
+    
+    if (readData != *(data + i)) {
+      digitalWrite(ERROR_LED, true);
+      Serial.print("-WRITE CHECK ERROR: ADDRESS ");
+      Serial.print(address + i, HEX);
+      Serial.print(" EXPECTED ");
+      Serial.print(*(data + i), HEX);
+      Serial.print(" BUT READ ");
+      Serial.println(readData, HEX);
+      digitalWrite(OUTPUT_ENABLE_PIN, true);
+      chipEnable(false);
+      return;
+    }
+  }
+  Serial.println("+");
+  digitalWrite(OUTPUT_ENABLE_PIN, true);
+  chipEnable(false);  
+}
 
 void selectAddress(int addr) {
   PAGE_BUS_O = (addr >> 8) & 0x001f;
   ADDRESS_BUS_O = addr & 0x00ff;
 }
-
 
 void chipEnable(bool state) {
   digitalWrite(CHIP_ENABLE_PIN, !state);
