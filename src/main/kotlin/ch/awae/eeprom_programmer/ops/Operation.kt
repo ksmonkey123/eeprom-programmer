56e2584511ef3da2fbd87d43f1a58d225fa8401b
Andreas Wälchli <andreas.waelchli@me.com>
2025-11-11 11:09:36 +0000

    continued CLI

--- src/main/kotlin/ch/awae/eeprom_programmer/ops/Operation.kt
+++
@@ -1,7 +0,0 @@
-package ch.awae.eeprom_programmer.ops
-
-abstract class Operation(val command: String, val helpText: String, vararg val paramLabels: String) {
-
-    abstract fun execute(args: List<String>)
-
-}
